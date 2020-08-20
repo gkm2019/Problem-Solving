6bea32f0180f626422dd63cb0a44d39ba1155b45
wlsdhr0831 <37607649+wlsdhr0831@users.noreply.github.com>
2020-08-20 19:56:49 +0900

    Update N17071.java

--- 8월 3주차/숨바꼭질 5/성진옥/N17071.java
+++ 8월 3주차/숨바꼭질 5/성진옥/N17071.java
@@ -10,8 +10,8 @@ import java.util.StringTokenizer;
 
 public class N17071 {
 	static int N, K;
-	static int dir[][] = {{1,-1}, {1,1}, {2,0}};
-	static boolean visit[][] = new boolean[2][500001];
+	static int dir[][] = {{1,-1}, {1,1}, {2,0}}; // 이동할 수 있는 장소
+	static boolean visit[][] = new boolean[2][500001]; // 방문여부 저장
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,30 +23,30 @@ public class N17071 {
 	}
 	
 	private static int bfs() {
-		if(N == K) return 0;
+		if(N == K) return 0; // 출발점과 도착점이 같으면
 		
-		int cnt = 0;
+		int cnt = 0; // 동생의 이동량 저장
 		Queue<Integer> q = new LinkedList<>();
-		visit[0][N] = true;
-		q.add(N);
+		visit[0][N] = true; // 언니의 출발점 visit 체크
+		q.add(N); // 언니 현재 위치 저장
 		
 		while(!q.isEmpty()) {
-			int size = q.size();
+			int size = q.size(); // 같은 날에 도착하는 곳 먼저 체크하기
 			
-			K += (++cnt);
-			if(K > 500000) break;
-			if(visit[cnt%2][K]) return cnt;
+			K += (++cnt); // 동생의 위치 계산
+			if(K > 500000) break; // 범위 벗어나면 끝
+			if(visit[cnt%2][K]) return cnt; // 동생이 도착하는 곳이 이미 방문했던 곳이면 종료
 			
-			for(int s = 0; s < size; s++) {
+			for(int s = 0; s < size; s++) { // 언니가 갈 수 있는 곳 체크
 				int cur = q.poll();
 				
 				for(int i = 0; i < 3; i++) {
-					int next = cur * dir[i][0] + dir[i][1];
+					int next = cur * dir[i][0] + dir[i][1]; // 다음 이동장소
 					
-					if(next < 0 || next > 500000 || visit[cnt%2][next]) continue;
-					if(next == K) return cnt;
+					if(next < 0 || next > 500000 || visit[cnt%2][next]) continue; // 범위 밖이거나 이미 방문한적 있으면 패스
+					if(next == K) return cnt; // 언니와 동생이 만나면 종료
 					
-					visit[cnt%2][next] = true;
+					visit[cnt%2][next] = true; // 현재 위치 방문 체크
 					q.add(next);
 				}				
 			}
